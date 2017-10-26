f244f92431d5f1e32fb24cabfcefa7e6ddba7426
Allan <brunskillage@gmail.com>
2017-10-26 22:41:25 +0100

    Cleanup

--- OneCopy2017/DataObjects/CommandArgumentsStrategyOption.cs
+++ OneCopy2017/DataObjects/CommandArgumentsStrategyOption.cs
@@ -1,11 +1,5 @@
 namespace OneCopy2017.DataObjects
 {
-    using System.Runtime.CompilerServices;
-
-    namespace OneCopy2017.DataObjects
-    {
-    }
-
     public enum CommandArgumentsStrategyOption
     {
         Oldest, Newest
